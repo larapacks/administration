4d244fd6fe44619d4f2935b94bf791e4064ec8b4
Steve Bauman <steven_bauman@outlook.com>
2016-09-12 12:17:39 -0400

    Don't allow users to change the administrators name

--- resources/views/roles/form.blade.php
+++ resources/views/roles/form.blade.php
@@ -2,7 +2,25 @@
 
     <label class="help-block">Name</label>
 
-    <input name="name" type="text" class="form-control" placeholder="Enter a role name." value="{{ old('name', isset($role) ? $role->name : null) }}">
+    @if(isset($role) && $role->isAdministrator())
+
+        <div class="alert alert-warning alert-important">
+            <p>You cannot change the administrators role name.</p>
+        </div>
+
+
+        <input name="name" type="hidden" value="{{ $role->name }}">
+
+    @endif
+
+    <input
+            name="name"
+            type="text"
+            {{ isset($role) && $role->isAdministrator() ? 'disabled' : null }}
+            class="form-control"
+            placeholder="Enter a role name."
+            value="{{ old('name', isset($role) ? $role->name : null) }}"
+    >
 
     <p class="help-block">{{ $errors->first('name') }}</p>
 
