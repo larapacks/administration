fe5658d82edad84a9bd4945ce1bdf181dc0e3b4b
Steve Bauman <steven_bauman@outlook.com>
2016-09-14 11:55:27 -0400

    Use new config

--- src/Seeders/RoleAndPermissionSeeder.php
+++ src/Seeders/RoleAndPermissionSeeder.php
@@ -8,55 +8,19 @@ use Larapacks\Authorization\Authorization;
 class RoleAndPermissionSeeder extends Seeder
 {
     /**
-     * The roles to seed.
-     *
-     * @var array
-     */
-    protected $roles = [
-        [
-            'name'  => 'administrator',
-            'label' => 'Administrator',
-        ]
-    ];
-
-    /**
-     * The permissions to seed.
-     *
-     * @var array
-     */
-    protected $permissions = [
-        [
-            'name'  => 'admin',
-            'label' => 'Access Administration',
-        ],
-        [
-            'name'  => 'admin.users',
-            'label' => 'Manage Users',
-        ],
-        [
-            'name'  => 'admin.roles',
-            'label' => 'Manage Roles',
-        ],
-        [
-            'name'  => 'admin.permissions',
-            'label' => 'Manage Permissions',
-        ],
-    ];
-
-    /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        foreach ($this->roles as $role) {
+        foreach (config('admin.roles', []) as $role) {
             Authorization::role()->firstOrCreate($role);
         }
 
         $permissions = [];
 
-        foreach ($this->permissions as $permission) {
+        foreach (config('admin.permissions', []) as $permission) {
             $permissions[] = Authorization::permission()->firstOrCreate($permission);
         }
 
