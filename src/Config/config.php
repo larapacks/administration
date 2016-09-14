6f66dcea117762b24b3257d36ecf48d7d7324d68
Steve Bauman <steven_bauman@outlook.com>
2016-09-14 11:54:36 -0400

    Added stock roles and permissions to configuration

--- src/Config/config.php
+++ src/Config/config.php
@@ -25,4 +25,59 @@ return [
 
     'prefix' => 'admin',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Roles
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to configure the roles that are created upon
+    | setup. These roles will not be able to be deleted
+    | through the administrator interface.
+    |
+    | By default, the first role in this list will
+    | be given **all** permissions listed below.
+    |
+    */
+
+    'roles' => [
+        [
+            'name'  => 'administrator',
+            'label' => 'Administrator',
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permissions
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to configure the permissions that are created upon
+    | setup. These permissions will not be able to be deleted
+    | through the administrator interface.
+    |
+    | The below permission cannot be modified, but you may
+    | add as many new permissions as you'd like.
+    |
+    |
+    */
+
+    'permissions' => [
+        [
+            'name'  => 'admin',
+            'label' => 'Access Administration',
+        ],
+        [
+            'name'  => 'admin.users',
+            'label' => 'Manage Users',
+        ],
+        [
+            'name'  => 'admin.roles',
+            'label' => 'Manage Roles',
+        ],
+        [
+            'name'  => 'admin.permissions',
+            'label' => 'Manage Permissions',
+        ],
+    ]
+
 ];
